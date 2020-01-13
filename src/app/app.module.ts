4ebe8a039778f21ef166372c35e60bd3a6a83410
Ankita Gage <ankitagage95@gmail.com>
2020-01-13 11:11:44 +0530

    Add app.module.ts in zafar branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule
